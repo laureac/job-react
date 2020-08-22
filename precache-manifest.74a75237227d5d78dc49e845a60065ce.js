fc802edb1c861cf1c61ca5ec894609e2b4466528
Laure Claret <laure.a.claret@gmail.com>
2020-08-22 10:52:40 +1000

    Updates

---
+++ precache-manifest.74a75237227d5d78dc49e845a60065ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d62226dd311a3e92900d03d17d2137e",
+    "url": "/job-react/index.html"
+  },
+  {
+    "revision": "b1fae1aa6d3cb69a03fa",
+    "url": "/job-react/static/css/2.53823a3d.chunk.css"
+  },
+  {
+    "revision": "d2aae6e4da062be21387",
+    "url": "/job-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b1fae1aa6d3cb69a03fa",
+    "url": "/job-react/static/js/2.87b89e37.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/job-react/static/js/2.87b89e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2aae6e4da062be21387",
+    "url": "/job-react/static/js/main.2c43dde8.chunk.js"
+  },
+  {
+    "revision": "171e15b62e95de993dea",
+    "url": "/job-react/static/js/runtime-main.42a291b5.js"
+  }
+]);
